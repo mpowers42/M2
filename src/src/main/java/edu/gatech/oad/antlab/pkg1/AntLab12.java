4f1916c3878ce1d5a900f864d62f67761d054e1c
Morgan Powers <powers.morgan@ymail.com>
2019-05-29 21:16:15 -0400

    Initial commit

---
+++ src/src/main/java/edu/gatech/oad/antlab/pkg1/AntLab12.java
@@ -0,0 +1,21 @@
+package src.main.java.edu.gatech.oad.antlab.pkg1;
+
+
+
+/** 
+ * CS2335 Ant Lab
+ *
+ * AntLab12.java helper class
+ */
+ public class AntLab12 {
+    
+    
+   /**
+    * retrieves a pre-stored string message 
+    * @return the string
+    */
+    public String getMessage() {
+        return " You";
+    }
+    
+ } 
